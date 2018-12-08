46265cbd445ac5374e86e68e9b2f6207504efeb1
osozero <sametozer13@gmail.com>
2018-12-08 01:24:55 -0800

    Texture class interface implemented.

--- breakout/src/Texture.h
+++ breakout/src/Texture.h
@@ -1,8 +1,32 @@
 #pragma once
+
+#include <GL/glew.h>
+
 class Texture
 {
 public:
+	GLuint ID;
+
+	//dimensions
+	GLuint Width, Height;
+
+	//format
+	GLuint InternalFormat;
+	GLuint ImageFormat;
+
+	//configs
+	GLuint WrapS;
+	GLuint WrapT;
+	GLuint FilterMin;
+	GLuint FilterMax;
+
+	void Generate(GLuint _width, GLuint _height, unsigned char* data);
+
+	void Bind() const;
+
 	Texture();
 	~Texture();
+
+
 };
 
